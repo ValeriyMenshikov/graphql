7b4b56c1936cd2f08bcea507b369fe0ae43fa6a3
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2023-08-10 11:01:31 +0300

    - Генерация схемы клиента - Использование схемы для работы с graphQL API

--- test_accounts.py
+++ test_accounts.py
@@ -1,34 +1,23 @@
 from sgqlc.endpoint.http import HTTPEndpoint
+from sgqlc.operation import Operation
+from schema import PagingQueryInput, Query, AccountsResponse
+import schema
 import pprint
 
 
 def test_accounts():
     url = 'http://localhost:5051/graphql'
-
-    query = '''
-    query($paging: PagingQueryInput) {
-      accounts(withInactive: true, paging: $paging) {
-          users {
-              login,
-              email,
-              location
-              isAuthenticated
-          },
-          paging {
-              currentPage,
-              totalPagesCount,
-              totalEntitiesCount
-          }
-      }
-    }
-    '''
-    variables = {
-        "paging": {
-            "size": 10,
-            "skip": 0
-        }
-    }
+    paging = PagingQueryInput(
+        skip=0,
+        number=0,
+        size=10
+    )
+    query = Operation(schema.Query, 'accounts')
+    query.accounts(paging=paging, with_inactive=True)
+    # query.accounts.__fields__(users=True, paging=True)
+    query.accounts.users.__fields__('login', 'email', 'is_authenticated')
+    print(query)
 
     endpoint = HTTPEndpoint(url)
-    data = endpoint(query, variables)
+    data = endpoint(query)
     pprint.pprint(data)
--- test_register_account.py
+++ test_register_account.py
@@ -1,25 +1,21 @@
 from sgqlc.endpoint.http import HTTPEndpoint
+from sgqlc.operation import Operation
+
+import schema
+from schema import Mutation, RegistrationInput, AccountRegisterResponse
 import pprint
 
 
 def test_register_account():
     url = 'http://localhost:5051/graphql'
-    mutation = '''
-    mutation registerAccount($registration: RegistrationInput) {
-      registerAccount(registration: $registration) {
-          id,
-          login
-      }
-    }
-        '''
-    variables = {
-        "registration": {
-            "email": "valeriy_menshikov@mail.ru",
-            "login": "valeriy_menshikov",
-            "password": "valeriy_menshikov"
-        }
-    }
-
+    mutation = Operation(schema.Mutation, 'registerAccount')
+    mutation_query = RegistrationInput(
+        email="valeriy_menshikov1@mail.ru",
+        login="valeriy_menshikov1",
+        password="valeriy_menshikov1"
+    )
+    mutation.register_account(registration=mutation_query)
+    print(mutation)
     endpoint = HTTPEndpoint(url)
-    data = endpoint(mutation, variables)
+    data = endpoint(mutation)
     pprint.pprint(data)
