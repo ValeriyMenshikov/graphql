016e7798816f6b7f98b30ba79677f3d1e857ed70
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2023-08-16 19:09:11 +0300

    Сырой клиент на почтовый сервер

---
+++ mailhog_api/mailhog_api.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from time import sleep
+
+
+class MailhogApi:
+    def __init__(self, host: str, disable_log=False) -> None:
+        self.host = host
+        self.disable_log = disable_log
+        self.client = requests.session()
+
+    def get_api_v2_messages(self, limit: int = 50) -> requests.Response:
+        """
+        Get messages by limit
+        :param limit:
+        :return:
+        """
+        response = self.client.get(
+            url=f"{self.host}/api/v2/messages",
+            params={
+                'limit': limit
+            }
+        )
+
+        return response
+
+    def get_token_from_last_email(self) -> str:
+        """
+        Get user activation token from last email
+        :return:
+        """
+        sleep(2)
+        emails = self.get_api_v2_messages(limit=1).json()
+        data = json.loads(emails["items"][0]["Content"]["Body"])
+        token_url = data.get("ConfirmationLinkUrl") or data.get("ConfirmationLinkUri")
+        token = token_url.split("/")[-1]
+        return token
--- tests/graphql/test_register_account.py
+++ tests/graphql/test_register_account.py
@@ -1,6 +1,7 @@
 import structlog
 from account_api.account_api import GraphQLAccountApiClient
-from account_api.schema import RegistrationInput
+from account_api.schema import RegistrationInput, UUID
+from mailhog_api.mailhog_api import MailhogApi
 
 structlog.configure(
     processors=[
@@ -13,7 +14,7 @@ def test_register_account():
     client = GraphQLAccountApiClient(
         host='http://5.63.153.31:5051/graphql',
     )
-
+    mailhog = MailhogApi('http://5.63.153.31:5025')
     email = "valeriy_menshikov2@mail.ru"
     login = "valeriy_menshikov2"
     password = "valeriy_menshikov2"
@@ -23,3 +24,5 @@ def test_register_account():
         password=password,
     )
     client.register_account(registration=registration)
+    activation_token = mailhog.get_token_from_last_email()
+    client.activate_account(activation_token=UUID(activation_token))
